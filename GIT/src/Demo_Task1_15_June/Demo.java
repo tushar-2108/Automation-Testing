d7bb1e0f6d0eb9ed1649adaa5230291a2a523cf4
DELL <DELL@DESKTOP-FLOKA02>
2022-06-17 13:34:32 +0530

    By TRS

--- GIT/src/Demo_Task1_15_June/Demo.java
+++ GIT/src/Demo_Task1_15_June/Demo.java
@@ -2,6 +2,11 @@ package Demo_Task1_15_June;
 
 public class Demo 
 {
+	
+	public void m1()
+	{
+		System.out.println("Hello tushar This m1");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("GIT Demo...");
